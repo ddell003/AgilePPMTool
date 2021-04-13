1ad2f9d632fe0b9d456cd4b577241859c6767487
Parker Dell <parker.dell@maxar.com>
2021-04-13 17:31:51 -0400

    added ui

--- pmbuilderApi/src/main/java/io/parkerdell/ppmtool/services/ProjectService.java
+++ pmbuilderApi/src/main/java/io/parkerdell/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.parkerdell.ppmtool.services;
 
 import io.parkerdell.ppmtool.domain.Project;
+import io.parkerdell.ppmtool.exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import io.parkerdell.ppmtool.repositories.ProjectRepository;
@@ -12,6 +13,32 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '"+ project.getProjectIdentifier()+"'already exists");
+        }
+
+    }
+
+    public Project findByProjectIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = findByProjectIdentifier(projectid);
+        projectRepository.delete(project);
     }
 }
--- pmbuilderApi/src/main/java/io/parkerdell/ppmtool/web/ProjectController.java
+++ pmbuilderApi/src/main/java/io/parkerdell/ppmtool/web/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -20,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
@@ -37,4 +35,26 @@ public class ProjectController {
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?>getProjectById(@PathVariable String projectId){
+        Project project = projectService.findByProjectIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public Iterable<Project>getAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/projectId")
+    public ResponseEntity<?>deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+    }
+
+
+
 }
